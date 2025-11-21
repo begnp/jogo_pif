b9dff42746fa74e4d7a7ecbf9b68d7b3f7d1a5e1
Caua Parente <cauahenriqueccpar@gmail.com>
2025-11-21 12:06:12 -0300

    Adicionado novas funcoes em map.c para carregamentos de plataformas e areas

--- src/map.c
+++ src/map.c
@@ -2,29 +2,69 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void InitMap(Map *map) {
-    map->platformsCount = 8 ;
-    map->platforms = (Rectangle *) malloc(sizeof(Rectangle) * map->platformsCount);
+void UnloadCurrentPlatforms(Map *map) {
+    if (map->platforms != NULL) {
+        free(map->platforms);
+        map->platforms = NULL;
+    }
+    map->platformsCount = 0;
+}
 
+void LoadArea(Map *map, int areaIndex) {
+    UnloadCurrentPlatforms(map);
+    map->currentArea = areaIndex;
+
+    switch (areaIndex) {
+        case 1: 
+            map->platformsCount = 8;
+            map->platforms = (Rectangle *) malloc(sizeof(Rectangle) * map->platformsCount);
+
+            map->platforms[0] = (Rectangle){-150, 520, 1500, 150};
+            map->platforms[1] = (Rectangle){1450, 620, 350, 20};  
+            
+            map->platforms[2] = (Rectangle){1900, 520, 150, 150};
+            map->platforms[3] = (Rectangle){2050, 400, 150, 250}; 
+            map->platforms[4] = (Rectangle){2200, 520, 150, 40}; 
+            
+            map->platforms[5] = (Rectangle){1450, 420, 90, 20};
+            map->platforms[6] = (Rectangle){1740, 350, 240, 20}; 
+            map->platforms[7] = (Rectangle){2100, 250, 200, 30};   
+
+            if (map->wallActive) {
+                map->breakableWall = (Rectangle){ 1350, 420, 100, 220 };
+            } else {
+                map->breakableWall = (Rectangle){ 0, 0, 0, 0 };
+            }
+            break;
+
+        case 2:
+            map->platformsCount = 3;
+            map->platforms = (Rectangle *) malloc(sizeof(Rectangle) * map->platformsCount);
+            map->platforms[0] = (Rectangle){0, 500, 1000, 100};
+            map->platforms[1] = (Rectangle){300, 400, 200, 20};
+            map->platforms[2] = (Rectangle){600, 300, 200, 20};
+            break;
+
+        default:
+            printf("Erro: Area %d nao existe!\n", areaIndex);
+            break;
+    }
+}
+
+void InitMap(Map *map) {
     map->texBackground = LoadTexture("assets/1.png");
     map->texForeground = LoadTexture("assets/0.png");
-    
     map->texTile = LoadTexture("assets/CaveTileset.png");
 
     if (map->texTile.id == 0) printf("Aviso: CaveTileset.png nao encontrado.\n"); 
 
     map->colorBorder = (Color){ 20, 20, 20, 255 };
 
-    map->platforms[0] = (Rectangle){-150, 520, 1500, 150};
-    map->platforms[1] = (Rectangle){1450, 620, 350, 20};
-    map->platforms[2] = (Rectangle){1900, 520, 150, 150};
-    map->platforms[3] = (Rectangle){2050, 400, 150, 250}; 
-    map->platforms[4] = (Rectangle){2200, 520, 150 , 40}; 
-    map->platforms[5] = (Rectangle){1450, 420, 90, 20};
-    map->platforms[6] = (Rectangle){1740, 350, 240, 20}; 
-    map->platforms[7] = (Rectangle){2100, 250, 200, 30};   
+    map->wallActive = true;
+    map->wallHealth = 3;
 
-    
+    map->platforms = NULL;
+    LoadArea(map, 1);
 }
 
 void DrawMapBackground(Map *map) {
@@ -35,6 +75,10 @@ void DrawMapBackground(Map *map) {
     Rectangle sourceFg = { 0.0f, 0.0f, (float)map->texForeground.width, (float)map->texForeground.height };
     Rectangle destFg = { 0.0f, 0.0f, (float)MAP_WIDTH, (float)MAP_HEIGHT };
     DrawTexturePro(map->texForeground, sourceFg, destFg, (Vector2){0,0}, 0.0f, WHITE);
+}
+
+void DrawTileRect(Texture2D texture, Rectangle dest, Rectangle source) {
+    DrawTexturePro(texture, source, dest, (Vector2){0,0}, 0.0f, WHITE);
 }
 
 void DrawMapPlatforms(Map *map) {
@@ -52,38 +96,47 @@ void DrawMapPlatforms(Map *map) {
                         float drawHeight = (y + tileSize > plat.height) ? (plat.height - y) : tileSize;
 
                         Rectangle source = { 32.0f, 96.0f, drawWidth, drawHeight };
-                        
+
                         if (y == 0) source.y = 64.0f; 
 
                         Rectangle dest = { plat.x + x, plat.y + y, drawWidth, drawHeight };
-                        DrawTexturePro(map->texTile, source, dest, (Vector2){0,0}, 0.0f, WHITE);
+                        DrawTileRect(map->texTile, dest, source);
                     }
                 }
-
-                float fadeStart = 30.0f; 
-                
-                DrawRectangleGradientV(
-                    (int)plat.x, 
-                    (int)(plat.y + fadeStart), 
-                    (int)plat.width, 
-                    (int)(plat.height - fadeStart), 
-                    (Color){ 0, 0, 0, 0 },
-                    (Color){ 0, 0, 0, 255 } 
-                );
-                
+                DrawRectangleGradientV((int)plat.x, (int)(plat.y + 30), (int)plat.width, (int)(plat.height - 30), (Color){0,0,0,0}, (Color){0,0,0,255});
             }
             else {
                 for (float x = 0; x < plat.width; x += tileSize) {
                     float drawWidth = (x + tileSize > plat.width) ? (plat.width - x) : tileSize;
                     Rectangle source = { 32.0f, 0.0f, drawWidth, 32.0f };
                     Rectangle dest = { plat.x + x, plat.y, drawWidth, plat.height };
-                    DrawTexturePro(map->texTile, source, dest, (Vector2){0,0}, 0.0f, WHITE);
+                    DrawTileRect(map->texTile, dest, source);
                 }
             }
-            
-        } 
-        else {
+        } else {
             DrawRectangleRec(plat, DARKGRAY);
+        }
+    }
+
+    if (map->wallActive) {
+        Rectangle wall = map->breakableWall;
+        
+        if (map->texTile.id > 0) {
+            for (float x = 0; x < wall.width; x += tileSize) {
+                for (float y = 0; y < wall.height; y += tileSize) {
+                    float drawWidth = (x + tileSize > wall.width) ? (wall.width - x) : tileSize;
+                    float drawHeight = (y + tileSize > wall.height) ? (wall.height - y) : tileSize;
+
+                    Rectangle source = { 32.0f, 96.0f, drawWidth, drawHeight }; 
+                    Rectangle dest = { wall.x + x, wall.y + y, drawWidth, drawHeight };
+                    
+                    DrawTexturePro(map->texTile, source, dest, (Vector2){0,0}, 0.0f, LIGHTGRAY);
+                    
+                    DrawRectangleLinesEx(dest, 1, (Color){0,0,0,50});
+                }
+            }
+        } else {
+            DrawRectangleRec(wall, BROWN);
         }
     }
 }
@@ -92,9 +145,5 @@ void UnloadMap(Map *map) {
     UnloadTexture(map->texBackground);
     UnloadTexture(map->texForeground);
     UnloadTexture(map->texTile); 
-
-    if (map->platforms != NULL) {
-        free(map->platforms);
-        map->platforms = NULL;
-    }
+    UnloadCurrentPlatforms(map);
 }
