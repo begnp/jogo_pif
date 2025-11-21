0a6f8d8d16730dc96da1b6c72d52e1673823b144
Caua Parente <cauahenriqueccpar@gmail.com>
2025-11-21 17:23:11 -0300

    Update na camera

--- src/camera.c
+++ src/camera.c
@@ -9,10 +9,25 @@ Camera2D InitCamera(Vector2 target, Vector2 offset) {
     return camera;
 }
 
-void UpdateCameraToFollowPlayer(Camera2D *camera, Vector2 playerPos, int screenWidth, int screenHeight) {
+void UpdateCameraToFollowPlayer(Camera2D *camera, Vector2 playerPos, int screenWidth, int screenHeight, float mapWidth, float mapHeight) {
     Vector2 newTarget;
     newTarget.x = playerPos.x + 32;
     newTarget.y = playerPos.y - 50;
+
+    float viewWidth = screenWidth / camera->zoom;
+    float viewHeight = screenHeight / camera->zoom;
+
+    float minX = viewWidth / 2.0f;
+    float minY = viewHeight / 2.0f;
+    
+    float maxX = mapWidth - (viewWidth / 2.0f);
+    float maxY = mapHeight - (viewHeight / 2.0f);
+
+    if (newTarget.x < minX) newTarget.x = minX;
+    if (newTarget.x > maxX) newTarget.x = maxX;
+
+    if (newTarget.y < minY) newTarget.y = minY;
+    if (newTarget.y > maxY) newTarget.y = maxY;
 
     camera->target = newTarget;
 }
